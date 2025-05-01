4349ccbf763f4561f05d044bd3c3dda673541ca1
m00nbek <65346086+m00nbek@users.noreply.github.com>
2025-05-01 18:51:25 +0500

    init proj

---
+++ Drinker/Drinker/DrinkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkerApp.swift
+//  Drinker
+//
+//  Created by nigga on 01/05/25.
+//
+
+import SwiftUI
+
+@main
+struct DrinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
